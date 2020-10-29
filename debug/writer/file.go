1e8f5a438cf416d4dc9f9c05b4f9149dc509acd8
linwenxiang <991154416@qq.com>
2020-10-29 17:08:59 +0800

    默认打开日志文件

--- debug/writer/file.go
+++ debug/writer/file.go
@@ -8,6 +8,9 @@ import (
 	"time"
 )
 
+// FileNameTimeFormat 文件名称格式
+var FileNameTimeFormat = "2006-01-02"
+
 // FileWriter 文件写入结构体
 type FileWriter struct {
 	path   string
@@ -17,11 +20,17 @@ type FileWriter struct {
 }
 
 // NewFileWriter 实例化FileWriter
-func NewFileWriter(path, suffix string) *FileWriter {
+func NewFileWriter(path, suffix string) (*FileWriter, error) {
+	filename := filepath.Join(path, fmt.Sprintf("%s.%s", time.Now().Format(FileNameTimeFormat), suffix))
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE|os.O_SYNC, 0600)
+	if err != nil {
+		return nil, err
+	}
 	return &FileWriter{
 		path:   path,
 		suffix: suffix,
-	}
+		file:   file,
+	}, nil
 }
 
 // Write 写入方法
@@ -35,7 +44,7 @@ func (p *FileWriter) Write(data []byte) (n int, err error) {
 	n, e := p.file.Write(data)
 	p.size += int64(n)
 	//每天一个文件
-	filename := filepath.Join(p.path, fmt.Sprintf("%s.%s", time.Now().Format("2006-01-02"), p.suffix))
+	filename := filepath.Join(p.path, fmt.Sprintf("%s.%s", time.Now().Format(FileNameTimeFormat), p.suffix))
 	if p.file.Name() != filename {
 		_ = p.file.Close()
 		p.file, _ = os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE|os.O_SYNC, 0600)
