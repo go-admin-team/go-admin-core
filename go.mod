b289a9f0f1738a4c5d884a6ca74b1cfae57482dc
linwenxiang <991154416@qq.com>
2021-03-05 11:48:35 +0800

    新增go-admin运行时工具支持

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/bitly/go-simplejson v0.5.0
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/bsm/redislock v0.5.0
+	github.com/casbin/casbin/v2 v2.24.0
 	github.com/fsnotify/fsnotify v1.4.7
 	github.com/ghodss/yaml v1.0.0
 	github.com/gin-gonic/gin v1.6.3
@@ -16,6 +17,7 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/imdario/mergo v0.3.9
 	github.com/pkg/errors v0.9.1
+	github.com/robfig/cron/v3 v3.0.1
 	github.com/robinjoseph08/redisqueue/v2 v2.1.0
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/smartystreets/goconvey v1.6.4
---
+++ tools/runtime/type.go
@@ -0,0 +1,33 @@
+package runtime
+
+import (
+	"net/http"
+
+	"github.com/casbin/casbin/v2"
+	"github.com/go-admin-team/go-admin-core/logger"
+	"github.com/robfig/cron/v3"
+	"gorm.io/gorm"
+)
+
+type Runtime interface {
+	//多db设置，⚠️SetDbs不允许并发,可以根据自己的业务，例如app分库、host分库
+	SetDb(key string, db *gorm.DB)
+	GetDb() map[string]*gorm.DB
+	GetDbByKey(key string) *gorm.DB
+
+	SetCasbin(key string, enforcer *casbin.SyncedEnforcer)
+	GetCasbinKey(key string) *casbin.SyncedEnforcer
+
+	//使用的路由
+	SetEngine(engine http.Handler)
+	GetEngine() http.Handler
+
+	//使用go-admin定义的logger，参考来源go-micro
+	SetLogger(logger logger.Logger)
+	GetLogger() logger.Logger
+
+	//crontab
+	SetCrontab(key string, crontab *cron.Cron)
+	GetCrontab() map[string]*cron.Cron
+	GetCrontabKey(key string) *cron.Cron
+}
