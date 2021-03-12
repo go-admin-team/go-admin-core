894c7ed987f22e57ca29484e560082560d25b2ce
linwenxiang <991154416@qq.com>
2021-03-12 15:58:32 +0800

    优化包引用名称，增加获取logger方法

---
+++ sdk/api/request_logger.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-admin-team/go-admin-core/sdk"
+	"github.com/go-admin-team/go-admin-core/sdk/pkg"
+	"github.com/go-admin-team/go-admin-core/sdk/pkg/logger"
+)
+
+// GetRequestLogger 获取上下文提供的日志
+func GetRequestLogger(c *gin.Context) *logger.Logger {
+	requestId := pkg.GenerateMsgIDFromContext(c)
+	log := sdk.Runtime.GetLogger().Fields(map[string]interface{}{
+		strings.ToLower(pkg.TrafficKey): requestId,
+	})
+	return &logger.Logger{Logger: log}
+}
