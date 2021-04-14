2c55f738ec26bfaaf4dfd8fcd24bdfd42ff07592
wenjianzhang <zwj777@live.com>
2021-04-14 16:57:30 +0800

    fix🐛 ： post 函数添加 error 返回

--- sdk/pkg/http.go
+++ sdk/pkg/http.go
@@ -36,17 +36,18 @@ func Get(url string) (string, error) {
 // data：        POST请求提交的数据
 // contentType： 请求体格式，如：application/json
 // content：     请求放回的内容
-func Post(url string, data interface{}, contentType string) string {
+func Post(url string, data interface{}, contentType string) ([]byte, error) {
 
 	// 超时时间：5秒
 	client := &http.Client{Timeout: 5 * time.Second}
 	jsonStr, _ := json.Marshal(data)
 	resp, err := client.Post(url, contentType, bytes.NewBuffer(jsonStr))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	result, _ := ioutil.ReadAll(resp.Body)
-	return string(result)
+	return result, nil
+
 }
