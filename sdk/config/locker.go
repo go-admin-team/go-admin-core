b42693c49c6ceb8a9ecc373e1796f621cb03b07c
linwenxiang <991154416@qq.com>
2021-04-27 09:57:51 +0800

    feat :sparkles: sdk 增加queue locker配置空判断

--- sdk/config/locker.go
+++ sdk/config/locker.go
@@ -12,6 +12,11 @@ type Locker struct {
 	Redis *RedisConnectOptions
 }
 
+// Empty 空设置
+func (e Locker) Empty() bool {
+	return e.Redis == nil
+}
+
 // Setup 启用顺序 redis > 其他 > memory
 func (e Locker) Setup() (storage.AdapterLocker, error) {
 	if e.Redis != nil {
