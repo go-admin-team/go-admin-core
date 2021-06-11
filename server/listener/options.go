3c4392a5787ebabceae80c823fc2ebda426df8eb
linwenxiang <991154416@qq.com>
2021-06-11 16:32:12 +0800

    feature :sparkles: 优化server manager

--- server/listener/options.go
+++ server/listener/options.go
@@ -17,14 +17,29 @@ type Option func(*options)
 type options struct {
 	addr, certFile, keyFile string
 	handler                 http.Handler
+	startedHook             func()
+	endHook                 func()
 }
 
 func setDefaultOption() options {
 	return options{
 		addr: ":8080",
 		handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			_, _ = w.Write([]byte("ok"))
+			w.WriteHeader(http.StatusOK)
 		}),
+	}
+}
+
+func WithEndHook(f func()) Option {
+	return func(o *options) {
+		o.endHook = f
+	}
+}
+
+// WithStartedHook 设置启动回调函数
+func WithStartedHook(f func()) Option {
+	return func(o *options) {
+		o.startedHook = f
 	}
 }
 
