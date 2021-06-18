0668bb71542c35514d7a89222d25a78cf4397b2c
linwenxiang <991154416@qq.com>
2021-06-18 15:11:55 +0800

    bugfix :bug: 修复logger file提前关闭问题，异步写入

--- debug/writer/file.go
+++ debug/writer/file.go
@@ -3,21 +3,23 @@ package writer
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
-// FileNameTimeFormat 文件名称格式
-var FileNameTimeFormat = "2006-01-02"
+// fileNameTimeFormat 文件名称格式
+const fileNameTimeFormat = "2006-01-02"
 
 // FileWriter 文件写入结构体
 type FileWriter struct {
 	file         *os.File
-	size         uint
 	FilenameFunc func(*FileWriter) string
 	num          uint
-	opts         options
+	opts         Options
+	input        chan []byte
 }
 
 // NewFileWriter 实例化FileWriter, 支持大文件分割
@@ -32,12 +34,11 @@ func NewFileWriter(opts ...Option) (*FileWriter, error) {
 	var err error
 	for {
 		filename = p.getFilenameAccordingToTimestamp()
-		info, err := os.Stat(filename)
+		_, err := os.Stat(filename)
 		if err != nil {
 			if os.IsNotExist(err) {
-				if p.size < p.opts.cap && p.num > 0 {
+				if p.num > 0 {
 					p.num--
-					fmt.Println(p.size, p.opts.cap, p.num)
 					filename = p.getFilenameAccordingToTimestamp()
 				}
 				//文件不存在
@@ -46,7 +47,6 @@ func NewFileWriter(opts ...Option) (*FileWriter, error) {
 			//存在，但是报错了
 			return nil, err
 		}
-		p.size = uint(info.Size())
 		p.num++
 		if p.opts.cap == 0 {
 			break
@@ -56,7 +56,38 @@ func NewFileWriter(opts ...Option) (*FileWriter, error) {
 	if err != nil {
 		return nil, err
 	}
+	p.input = make(chan []byte, 100)
+	go p.write()
 	return p, nil
+}
+
+func (p *FileWriter) write() {
+	for {
+		select {
+		case d := <-p.input:
+			_, err := p.file.Write(d)
+			if err != nil {
+				log.Printf("write file failed, %s\n", err.Error())
+			}
+			p.checkFile()
+		}
+	}
+}
+
+func (p *FileWriter) checkFile() {
+	info, _ := p.file.Stat()
+	if strings.Index(p.file.Name(), time.Now().Format(fileNameTimeFormat)) < 0 ||
+		(p.opts.cap > 0 && uint(info.Size()) > p.opts.cap) {
+		//生成新文件
+		if uint(info.Size()) > p.opts.cap {
+			p.num++
+		} else {
+			p.num = 0
+		}
+		filename := p.getFilenameAccordingToTimestamp()
+		_ = p.file.Close()
+		p.file, _ = os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE|os.O_SYNC, 0600)
+	}
 }
 
 // Write 写入方法
@@ -67,22 +98,11 @@ func (p *FileWriter) Write(data []byte) (n int, err error) {
 	if p.file == nil {
 		return 0, errors.New("file not opened")
 	}
-	n, e := p.file.Write(data)
-	p.size += uint(n)
-	//每天一个文件
-	filename := p.getFilenameAccordingToTimestamp()
-	if p.file.Name() != filename || (p.opts.cap > 0 && p.size > p.opts.cap) {
-		if p.size > p.opts.cap {
-			p.num += 1
-			filename = p.getFilenameAccordingToTimestamp()
-		} else {
-			p.num = 0
-		}
-		p.size = 0
-		_ = p.file.Close()
-		p.file, _ = os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE|os.O_SYNC, 0600)
-	}
-	return n, e
+	n = len(data)
+	go func() {
+		p.input <- data
+	}()
+	return n, nil
 }
 
 // getFilenameAccordingToTimestamp 通过日期命名log文件
@@ -93,12 +113,12 @@ func (p *FileWriter) getFilenameAccordingToTimestamp() string {
 	if p.opts.cap == 0 {
 		return filepath.Join(p.opts.path,
 			fmt.Sprintf("%s.%s",
-				time.Now().Format(FileNameTimeFormat),
+				time.Now().Format(fileNameTimeFormat),
 				p.opts.suffix))
 	}
 	return filepath.Join(p.opts.path,
 		fmt.Sprintf("%s-[%d].%s",
-			time.Now().Format(FileNameTimeFormat),
+			time.Now().Format(fileNameTimeFormat),
 			p.num,
 			p.opts.suffix))
 }
