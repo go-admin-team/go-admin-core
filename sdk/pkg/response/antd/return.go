d2605ed1233ebb511350d98f4ca8593f85dc59f0
zhangwenjian <zwj777@live.com>
2022-06-20 22:49:14 +0800

    refactor🎨：代码格式化

--- sdk/pkg/response/antd/return.go
+++ sdk/pkg/response/antd/return.go
@@ -28,6 +28,18 @@ func OK(c *gin.Context, data interface{}) {
 	var res response
 	res.Data = data
 	res.Success = true
+	res.Status = "done"
+	res.TraceId = pkg.GenerateMsgIDFromContext(c)
+	c.Set("result", res)
+	c.Set("status", http.StatusOK)
+	c.AbortWithStatusJSON(http.StatusOK, res)
+}
+
+func UpFileOK(c *gin.Context, data interface{}) {
+	var res response
+	res.Data = data
+	res.Success = true
+	res.Status = "done"
 	res.TraceId = pkg.GenerateMsgIDFromContext(c)
 	c.Set("result", res)
 	c.Set("status", http.StatusOK)
