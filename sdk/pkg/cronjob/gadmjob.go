6e2b55972c092fb0b914ab173d0f7074aacd5182
zhangwenjian <zwj777@live.com>
2022-09-05 20:32:07 +0800

    docs📝: Modify the description of newwithseconds method

--- sdk/pkg/cronjob/gadmjob.go
+++ sdk/pkg/cronjob/gadmjob.go
@@ -4,7 +4,7 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-// NewWithSeconds newWithSeconds returns a Cron with the seconds field enabled.
+// NewWithSeconds returns a Cron with the seconds field enabled.
 func NewWithSeconds() *cron.Cron {
 	secondParser := cron.NewParser(cron.Second | cron.Minute |
 		cron.Hour | cron.Dom | cron.Month | cron.DowOptional | cron.Descriptor)
