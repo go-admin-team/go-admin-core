c1dec45c44c4e3e2fe1f3d3139f68e1f6cc74280
wenjianzhang <zwj777@live.com>
2024-01-03 12:54:15 +0800

    refactor🎨: 追加新的验证码返回参数

--- sdk/pkg/captcha/captcha.go
+++ sdk/pkg/captcha/captcha.go
@@ -24,7 +24,7 @@ type configJsonBody struct {
 	DriverDigit   *base64Captcha.DriverDigit
 }
 
-func DriverStringFunc() (id, b64s string, err error) {
+func DriverStringFunc() (id, b64s, answer string, err error) {
 	e := configJsonBody{}
 	e.Id = uuid.New().String()
 	e.DriverString = base64Captcha.NewDriverString(46, 140, 2, 2, 4, "234567890abcdefghjkmnpqrstuvwxyz", &color.RGBA{240, 240, 246, 246}, nil, []string{"wqy-microhei.ttc"})
@@ -33,7 +33,7 @@ func DriverStringFunc() (id, b64s string, err error) {
 	return captcha.Generate()
 }
 
-func DriverDigitFunc() (id, b64s string, err error) {
+func DriverDigitFunc() (id, b64s, answer string, err error) {
 	e := configJsonBody{}
 	e.Id = uuid.New().String()
 	e.DriverDigit = base64Captcha.NewDriverDigit(80, 240, 4, 0.7, 80)
